7da5686d891b71dc82a291c3e3e631c980a89c88
lijj0812 <ajunjian@163.com>
2020-12-30 19:18:54 +0800

    20201230

--- TestCase/test_login.py
+++ TestCase/test_login.py
@@ -16,6 +16,7 @@ import requests
 
 
 class TestLogin:
+
     @allure.feature('serviceCould')
     @allure.severity('blocker')
     @allure.story('login')
@@ -43,8 +44,43 @@ class TestLogin:
         re_time = r.elapsed.total_seconds()
 #        print("接口响应时间==========", r.elapsed.total_seconds())
         print("text==========", r.text)
-        rep_code = r.status_code
-        assert test.assert_code(rep_code, 200)
+#        rep_code = r.status_code
+#        assert test.assert_code(rep_code, 200)
         assert test.assert_in_text(r.text, "access_token")
         Consts.STRESS_LIST.append(re_time)
         Consts.RESULT_LIST.append('True')
+
+
+'''
+    @allure.feature('serviceCould')
+    @allure.severity('blocker')
+    @allure.story('login')
+    def test_login_02(self, action):
+        """
+            用例描述：用户登录密码错误
+        """
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        conf = Config()
+        data = Login()
+        test = Assert.Assertions()
+#        request = Request.Request(action)
+
+        host = conf.host_release
+        req_url = 'https://' + host
+        urls = data.url
+        params = data.data
+        headers = data.header
+
+        api_url = req_url + urls[0]
+        datas = params[1][0]
+#        print("api_url==========", api_url)
+#        print("params==========", params[0][0])
+        r = requests.post(api_url, data=datas, verify=False)
+        re_time = r.elapsed.total_seconds()
+#        print("接口响应时间==========", r.elapsed.total_seconds())
+        print("text==========", r.text)
+#        rep_code = r.status_code
+#        assert test.assert_code(rep_code, 200)
+        assert test.assert_in_text(r.text, "access_token")
+        Consts.STRESS_LIST.append(re_time)
+'''
